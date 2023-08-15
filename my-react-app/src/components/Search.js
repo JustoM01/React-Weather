7f43f10599570af9e31366342ec5947574dab164
unknown <eliseo101801@gmail.com>
2023-08-15 13:08:37 -0700

    fixed issue rendering data

--- my-react-app/src/components/Search.js
+++ my-react-app/src/components/Search.js
@@ -29,37 +29,43 @@ const Search = () => {
   return (
     <div>
  
+   
+   
 
-    {weatherData && (
-        <div style={{backgroundColor:"white", position:"absolute", top:'18%', right:'35%', borderRadius:'30px', height:'550px', width:'550px'}}  >
        <input
 
-        style={{position:'absolute', right:"45%", top:'10px', fontFamily:'monospace', width:'250px',  border:'solid 3px black', height:'35px'}}
+        style={{position:'absolute', right:"50%", top:'116px', fontFamily:'monospace', width:'250px',  border:'solid 3px black', height:'35px'}}
         type="text"
         placeholder="Enter city"
         value={city}
         onChange={(e) => setCity(e.target.value)}
       />  
 
-      <Button variant="secondary"
 
-      style={{position:'absolute', right:"17%", top:'8px', fontFamily:'monospace', backgroundColor:'black'}}
-       onClick={fetchWeatherData}
-      >Get Weather</Button>
+   <Button variant="secondary"
 
-           
-          <h2   style={{position:'absolute', right:"22%", bottom:'74%', fontFamily:'monospace'}}>Weather in {weatherData.name}</h2>
-          <p style={{position:'absolute', bottom:'46%', right:'33%', fontSize:'30px', fontFamily:'monospace'}}>Temp {kelvinToFahrenheit(weatherData.main.temp).toFixed(2)}F </p>
-          <p style={{position:'absolute', bottom:'8%', right:'38%', fontSize:'25px', fontFamily:'monospace'}} >Weather: {weatherData.weather[0].description}</p>
-          <img  style={{position:'absolute', bottom:'2%', right:'9%', height:'130px'}}
-            src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`}
-            alt={weatherData.weather[0].description}
-          />
+    style={{position:'absolute', right:"40%", top:'115px', fontFamily:'monospace', backgroundColor:'black', width:'180px'}}
+    onClick={fetchWeatherData}
+    >Get Weather</Button>
 
 
+<div style={{backgroundColor:"white", position:"absolute", top:'18%', right:'35%', borderRadius:'30px', height:'550px', width:'550px'}}  >
+    {weatherData ? (
+        <>
+            <h2   style={{position:'absolute', right:"22%", bottom:'74%', fontFamily:'monospace'}}>Weather in {weatherData.name}</h2>
+            <p style={{position:'absolute', bottom:'46%', right:'33%', fontSize:'30px', fontFamily:'monospace'}}>Temp {kelvinToFahrenheit(weatherData.main.temp).toFixed(2)}F </p>
+            <p style={{position:'absolute', bottom:'8%', right:'38%', fontSize:'25px', fontFamily:'monospace'}} >Weather: {weatherData.weather[0].description}</p>
+            <img  style={{position:'absolute', bottom:'2%', right:'9%', height:'130px'}}
+                src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`}
+                alt={weatherData.weather[0].description}
+            />
+        </>
+    ) : (
+        <p style={{fontFamily:'monospace', position:'absolute', left:'25px'}}  >Awaiting City Request..</p>
+    )}
+</div>
 
-        </div>
-      )}
+
 
 </div>
  
